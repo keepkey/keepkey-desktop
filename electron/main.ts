85d05ac253df15a024a45add57bff6926f0ffb5c
highlander <bithighlander@gmail.com>
2021-12-04 21:54:23 -0700

    catch up

--- electron/main.ts
+++ electron/main.ts
@@ -1,38 +1,39 @@
-const { app, BrowserWindow, screen: electronScreen } = require('electron');
+// @ts-ignore
+const { app, BrowserWindow, screen: electronScreen } = require('electron')
 
 const createMainWindow = () => {
-    let mainWindow = new BrowserWindow({
-        width: electronScreen.getPrimaryDisplay().workArea.width,
-        height: electronScreen.getPrimaryDisplay().workArea.height,
-        show: false,
-        backgroundColor: 'white',
-        webPreferences: {
-            nodeIntegration: false
-        }
-    });
-    const startURL = 'http://localhost:3000';
+  let mainWindow = new BrowserWindow({
+    width: 460,
+    height: 780,
+    show: false,
+    backgroundColor: 'white',
+    webPreferences: {
+      nodeIntegration: true
+    }
+  })
+  const startURL = 'http://localhost:3000'
 
-    mainWindow.loadURL(startURL);
+  mainWindow.loadURL(startURL)
 
-    mainWindow.once('ready-to-show', () => mainWindow.show());
+  mainWindow.once('ready-to-show', () => mainWindow.show())
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
-};
+  mainWindow.on('closed', () => {
+    mainWindow = null
+  })
+}
 
 app.whenReady().then(() => {
-    createMainWindow();
+  createMainWindow()
 
-    app.on('activate', () => {
-        if (!BrowserWindow.getAllWindows().length) {
-            createMainWindow();
-        }
-    });
-});
+  app.on('activate', () => {
+    if (!BrowserWindow.getAllWindows().length) {
+      createMainWindow()
+    }
+  })
+})
 
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
