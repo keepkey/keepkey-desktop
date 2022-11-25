238b14f30c4326729dea0b57bab0f24b1f599ed9
highlander <bithighlander@gmail.com>
2022-11-24 23:41:51 -0600

    back to broke

--- electron/src/bridge/controllers/b-device-controller.ts
+++ electron/src/bridge/controllers/b-device-controller.ts
@@ -1,57 +1,60 @@
-import log from 'electron-log';
-import { windows } from '../../main';
-import { Body, Controller, Get, Post, Route, Tags, Security, Response } from 'tsoa';
-import { keepkey } from '..';
-import { Read, Error, WriteBody, Write } from '../types';
-import { app } from 'electron'
+import { Body, Controller, Get, Post, Route, Tags, Response } from 'tsoa';
+import { kkStateController, setDeviceBusyRead, setDeviceBusyWrite, deviceBusyRead, deviceBusyWrite } from '../../globalState';
+import { WriteBody } from '../types';
 
-//route
+export let lastReadTime = 0
+export let lastWriteTime = 0
+
+// returns length of time any current requests have been open to the device
+// helps us determine if we should close right away on exit or wait for current requests to finish
+export const checkIfStuck = () => {
+
+    let pendingReadLength = 0
+    let pendingWriteLength = 0
+
+    if(deviceBusyRead) pendingReadLength = Date.now() - lastReadTime
+    if(deviceBusyWrite) pendingWriteLength = Date.now() - lastWriteTime
+
+    return pendingReadLength > pendingWriteLength ? pendingReadLength : pendingWriteLength
+}
+
 @Tags('Raw KeepKey Device I/0 Endpoints')
 @Route('exchange')
 export class BDeviceController extends Controller {
-
-    private EVENT_LOG: Array<{ read: { data: string } }> = []
-
     @Get('device')
     // @Security("api_key")
     @Response(500, "Unable to communicate with device")
-    public async readDevice(): Promise<Read | Error> {
-        return new Promise<Read | Error>(async (resolve, reject) => {
-            if (!keepkey.transport) return reject({ success: false, reason: 'Unable to communicate with device' })
-            try{
-                let resp = await keepkey.transport.readChunk()
-                let output = {
-                    data: Buffer.from(resp).toString('hex')
-                }
-                // log.info('output: ', output)
-                this.EVENT_LOG.push({ read: output })
-                if (windows.mainWindow) windows.mainWindow.webContents.send('dataSent', { output })
-                return resolve(output)
-            }catch(e){
-                log.error("Hardware Error read: ",e)
-                //exit app
-                app.relaunch()
-                app.exit()
+    public async readDevice() {
+        try {
+            lastReadTime = Date.now()
+            setDeviceBusyRead(true)
+            // console.log('readDevice')
+            let resp = await kkStateController.transport?.readChunk() ?? ''
+            setDeviceBusyRead(false)
+            return {
+                data: Buffer.from(resp as any).toString('hex')
             }
-        })
+        } catch (e) {
+            setDeviceBusyRead(false)
+            throw(e)
+        }
     }
 
     @Post('device')
     // @Security("api_key")
     @Response(500, "Unable to communicate with device")
-    public async writeDevice(@Body() body: WriteBody): Promise<Write | Error> {
-        return new Promise<Write | Error>((resolve, reject) => {
-            try{
-                if (!keepkey.transport) return reject({ success: false, reason: 'Unable to communicate with device' })
-                let msg = Buffer.from(body.data, 'hex')
-                keepkey.transport.writeChunk(msg)
-                log.info('input: ', msg.toString('hex'))
-                // EVENT_LOG.push({ write: output })
-                if (windows.mainWindow) windows.mainWindow.webContents.send('dataReceive', { output: msg })
-                return resolve({ output: msg.toString() })
-            }catch(e){
-                log.error("Hardware Error write: ",e)
-            }
-        })
+    public async writeDevice(@Body() body: WriteBody) {
+        try {
+            lastWriteTime = Date.now()
+            setDeviceBusyWrite(true)
+            // console.log('writeDevice')
+            let msg = Buffer.from(body.data, 'hex') ?? ''
+            kkStateController.transport?.writeChunk(msg)
+            setDeviceBusyWrite(false)
+            return { output: msg.toString() }
+        } catch (e) {
+            setDeviceBusyWrite(false)
+            throw(e)
+        }
     }
 }
