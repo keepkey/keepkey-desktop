1bbdb2a6fe3efe2fa975af85bf5b69d68695c5cd
MrNerdHair <mrnerdhair@gmail.com>
2022-11-30 12:06:03 -0500

    delete some stuff we don't need

--- scripts/git-hooks/pre-push.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "Running linter prior to push..."
-yarn lint --fix
--- scripts/sha256sums.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-rm -f ./build/SHA256SUMS
-TEMP=$(mktemp)
-find -L ./build -type f | xargs shasum -a 256 | sed -r 's/^([0-9a-f]{64}\s+)\.\/build\//\1/' | LC_ALL=C sort -k 2 | tee "$TEMP"
-mv "$TEMP" ./build/SHA256SUMS
