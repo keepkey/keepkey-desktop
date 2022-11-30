4bf9d5c523410a92a2c45a2a16679ecf8b214d78
MrNerdHair <mrnerdhair@gmail.com>
2022-11-30 13:50:28 -0500

    remove useHasAppUpdated

--- packages/keepkey-desktop-app/src/App.tsx
+++ packages/keepkey-desktop-app/src/App.tsx
@@ -1,30 +1,16 @@
-import { Alert, AlertDescription } from '@chakra-ui/alert'
-import { Button } from '@chakra-ui/button'
-import type { ToastId } from '@chakra-ui/toast'
-import { useToast } from '@chakra-ui/toast'
 import { ipcRenderer } from 'electron'
-import { useEffect, useRef, useState } from 'react'
-import { FaSync } from 'react-icons/fa'
-import { useTranslate } from 'react-polyglot'
+import { useEffect, useState } from 'react'
 import { Routes } from 'Routes/Routes'
-import { IconCircle } from 'components/IconCircle'
 import type { PairingProps } from 'components/Modals/Pair/Pair'
 import { WalletActions } from 'context/WalletProvider/actions'
-import { useHasAppUpdated } from 'hooks/useHasAppUpdated/useHasAppUpdated'
 import { useModal } from 'hooks/useModal/useModal'
 import { useWallet } from 'hooks/useWallet/useWallet'
-import { logger } from 'lib/logger'
 
 export const App = () => {
   const {
     state: { deviceId },
     dispatch,
   } = useWallet()
-  const shouldUpdate = useHasAppUpdated()
-  const toast = useToast()
-  const toastIdRef = useRef<ToastId | null>(null)
-  const updateId = 'update-app'
-  const translate = useTranslate()
   const { setIsUpdatingKeepkey, state, disconnect } = useWallet()
 
   const { pair, sign, hardwareError, updateKeepKey, requestBootloaderMode, loading } = useModal()
@@ -153,32 +139,5 @@ export const App = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  useEffect(() => {
-    logger.debug({ shouldUpdate, updateId }, 'Update Check')
-    if (shouldUpdate && !toast.isActive(updateId)) {
-      const toastId = toast({
-        render: () => {
-          return (
-            <Alert status='info' variant='update-box' borderRadius='lg'>
-              <IconCircle boxSize={8} color='gray.500'>
-                <FaSync />
-              </IconCircle>
-              <AlertDescription ml={3}>{translate('updateToast.body')}</AlertDescription>
-
-              <Button colorScheme='blue' size='sm' onClick={() => window.location.reload()} ml={4}>
-                {translate('updateToast.cta')}
-              </Button>
-            </Alert>
-          )
-        },
-        id: updateId,
-        duration: null,
-        isClosable: false,
-        position: 'bottom-right',
-      })
-      if (!toastId) return
-      toastIdRef.current = toastId
-    }
-  }, [shouldUpdate, toast, translate])
   return <Routes />
 }
