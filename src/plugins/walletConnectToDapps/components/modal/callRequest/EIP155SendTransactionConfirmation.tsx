0510c48ba563149ba7450e30e9ca8361885f7219
Amitoj Singh <amitojsingh366@gmail.com>
2022-12-01 02:05:26 +0300

    fix stuff

--- src/plugins/walletConnectToDapps/components/modal/callRequest/EIP155SendTransactionConfirmation.tsx
+++ src/plugins/walletConnectToDapps/components/modal/callRequest/EIP155SendTransactionConfirmation.tsx
@@ -78,9 +78,13 @@ export const EIP155SendTransactionConfirmation = () => {
 
   useEffect(() => {
     if (!wallet) return
+    setLoading(true)
     const accounts = (wallet as KeepKeyHDWallet).ethGetAccountPaths({ coin: 'Ethereum', accountIdx: 0 })
     setAccountPath(accounts[0].addressNList);
-    (wallet as KeepKeyHDWallet).ethGetAddress({ addressNList: accounts[0].addressNList, showDisplay: false }).then(setAddress)
+    (wallet as KeepKeyHDWallet).ethGetAddress({ addressNList: accounts[0].addressNList, showDisplay: false }).then((accAddress)=>{
+      setLoading(false)
+      setAddress(accAddress)
+    })
   }, [wallet])
 
   useEffect(() => {
@@ -111,8 +115,8 @@ export const EIP155SendTransactionConfirmation = () => {
         }
 
         // if gasPrice was passed in it means we couldnt get maxPriorityFeePerGas & maxFeePerGas
-        if (txData.gasPrice) {
-          signData.gasPrice = txData.gasPrice
+        if (request.params[0].gasPrice) {
+          signData.gasPrice = request.params[0].gasPrice
           delete signData.maxPriorityFeePerGas
           delete signData.maxFeePerGas
         }
@@ -150,7 +154,7 @@ export const EIP155SendTransactionConfirmation = () => {
         setLoading(false)
       }
     },
-    [currentRequest.id, removeRequest, requests, toast, wallet, chainId, accountPath],
+    [currentRequest.id, removeRequest, request, toast, wallet, chainId, accountPath],
   )
 
   const onReject = useCallback(async () => {
