6a46b8ac022070b7ce3ad60426a78c1733c0dfb8
MrNerdHair <mrnerdhair@gmail.com>
2022-12-01 18:15:07 -0500

    fix new imports

--- .pnp.cjs
+++ .pnp.cjs
@@ -7929,7 +7929,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageLocation": "./.yarn/cache/@json-rpc-tools-types-npm-1.7.6-1c36bdb16d-f23ec7d79a.zip/node_modules/@json-rpc-tools/types/",\
           "packageDependencies": [\
             ["@json-rpc-tools/types", "npm:1.7.6"],\
-            ["keyvaluestorage-interface", "npm:1.0.0"]\
+            ["keyvaluestorage-interface", "npm:1.0.0"],\
+            ["tslib", "npm:2.4.1"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -7940,7 +7941,8 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
           "packageDependencies": [\
             ["@json-rpc-tools/utils", "npm:1.7.6"],\
             ["@json-rpc-tools/types", "npm:1.7.6"],\
-            ["@pedrouid/environment", "npm:1.0.1"]\
+            ["@pedrouid/environment", "npm:1.0.1"],\
+            ["tslib", "npm:2.4.1"]\
           ],\
           "linkType": "HARD"\
         }]\
@@ -12571,6 +12573,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@walletconnect/heartbeat", "npm:1.0.1"],\
             ["@walletconnect/events", "npm:1.0.1"],\
             ["@walletconnect/time", "npm:1.0.2"],\
+            ["events", "npm:3.3.0"],\
             ["tslib", "npm:1.14.1"]\
           ],\
           "linkType": "HARD"\
@@ -12623,6 +12626,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@walletconnect/jsonrpc-provider", "npm:1.0.6"],\
             ["@walletconnect/jsonrpc-utils", "npm:1.0.4"],\
             ["@walletconnect/safe-json", "npm:1.0.1"],\
+            ["events", "npm:3.3.0"],\
             ["tslib", "npm:1.14.1"]\
           ],\
           "linkType": "HARD"\
@@ -12658,6 +12662,7 @@ function $$SETUP_STATE(hydrateRuntimeState, basePath) {
             ["@walletconnect/jsonrpc-ws-connection", "npm:1.0.5"],\
             ["@walletconnect/jsonrpc-utils", "npm:1.0.4"],\
             ["@walletconnect/safe-json", "npm:1.0.1"],\
+            ["events", "npm:3.3.0"],\
             ["tslib", "npm:1.14.1"],\
             ["ws", "virtual:f815283df73bd6b891bbb32084b0074116b8acaeb36a167860acd6fc520ec0b6620871a6f54143638e5b71edc55c6687435936bc2f5da88ed28077d6dd897712#npm:7.5.9"]\
           ],\
