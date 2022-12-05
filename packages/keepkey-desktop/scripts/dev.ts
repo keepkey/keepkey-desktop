c0608b580028eb05cec13b444f158b6226c47872
Amitoj Singh <amitojsingh366@gmail.com>
2022-12-05 15:22:32 +0300

    change terminology

--- packages/keepkey-desktop/scripts/dev.ts
+++ packages/keepkey-desktop/scripts/dev.ts
@@ -34,11 +34,11 @@ export const dev = async () => {
   })()
 
   child.stdout.on('data', (data) => {
-    console.log(`stdout [electron renderer] : ${data}`);
+    console.log(`stdout [electron main] : ${data}`);
   })
 
   child.stderr.on('data', (data) => {
-    console.error(`stderr [electron renderer] : ${data}`);
+    console.error(`stderr [electron main] : ${data}`);
   })
 
   process.on('SIGINT', () => {
