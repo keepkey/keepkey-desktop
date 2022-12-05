89f1ddf2409afc7f521c7a5157681415e2c36114
MrNerdHair <mrnerdhair@gmail.com>
2022-12-05 14:22:09 -0500

    rest rework

--- packages/hdwallet-keepkey-rest/src/adapter.ts
+++ packages/hdwallet-keepkey-rest/src/adapter.ts
@@ -1,6 +1,6 @@
 import * as core from "@shapeshiftoss/hdwallet-core";
 
-import { KeepKeyRestHDWallet } from "./kkrest";
+import { KeepKeyRestHDWallet, KeepKeySdk } from "./kkrest";
 
 export class KkRestAdapter {
   keyring: core.Keyring;
@@ -20,9 +20,9 @@ export class KkRestAdapter {
     return Object.keys(this.keyring.wallets).length;
   }
 
-  public async pairDevice(sdk: any, queueIpcEvent: any): Promise<core.HDWallet> {
-    const wallet = new KeepKeyRestHDWallet(sdk, queueIpcEvent);
-    // await wallet.initialize();
+  public async pairDevice(sdk: KeepKeySdk): Promise<core.HDWallet> {
+    const wallet = await KeepKeyRestHDWallet.create(sdk);
+    await wallet.initialize();
     const deviceID = await wallet.getDeviceID();
     this.keyring.add(wallet, deviceID);
     this.currentDeviceID = deviceID;
