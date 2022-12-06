00d81bf7aee93a030dfe7d96a69aebfba72a4fdc
Amitoj Singh <amitojsingh366@gmail.com>
2022-12-07 02:21:00 +0300

    dont show unnecessary modals on create wallet

--- packages/keepkey-desktop-app/src/components/Modals/UpdateKeepKey/UpdateFirmware/UpdateFirmware.tsx
+++ packages/keepkey-desktop-app/src/components/Modals/UpdateKeepKey/UpdateFirmware/UpdateFirmware.tsx
@@ -34,10 +34,9 @@ export const UpdateFirmware = (params: any) => {
 
   useEffect(() => {
     console.log('params: ', params)
-    if (!params?.event?.bootloaderMode) {
-      requestBootloaderMode.open({ ...params.event })
-    }
-  }, [params?.event])
+    if (!params?.event?.bootloaderMode) requestBootloaderMode.open({ ...params.event })
+    else requestBootloaderMode.close()
+  }, [params, params.event, requestBootloaderMode])
 
   return (
     <ModalBody pt={5}>
