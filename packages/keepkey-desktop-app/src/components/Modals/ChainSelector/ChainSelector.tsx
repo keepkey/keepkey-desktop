11a9583d01d3d284ea92cca4424bf85387037c52
Amitoj Singh <amitojsingh366@gmail.com>
2022-12-12 22:27:24 +0300

    remove ununsed imports

--- packages/keepkey-desktop-app/src/components/Modals/ChainSelector/ChainSelector.tsx
+++ packages/keepkey-desktop-app/src/components/Modals/ChainSelector/ChainSelector.tsx
@@ -1,12 +1,7 @@
 import {
-  Alert,
-  AlertDescription,
-  AlertIcon,
   Box,
-  Button,
   Divider,
   Flex,
-  Image,
   Input,
   InputGroup,
   InputLeftElement,
@@ -18,7 +13,6 @@ import {
   ModalOverlay,
   Spinner,
   Stack,
-  Text as ChakraText,
 } from '@chakra-ui/react'
 // import { SessionTypes } from '@walletconnect/types'
 import { ipcRenderer } from 'electron-shim'
@@ -26,60 +20,12 @@ import { useCallback, useEffect, useState } from 'react'
 import { SlideTransition } from 'components/SlideTransition'
 import { Text } from 'components/Text'
 import { useModal } from 'hooks/useModal/useModal'
-import Client from '@pioneer-platform/pioneer-client'
-import { getConfig } from 'config'
 import { useForm, useWatch } from 'react-hook-form'
 import { SearchIcon } from '@chakra-ui/icons'
 import { getPioneerClient } from 'lib/getPioneerCleint'
-import { debounce } from 'lodash'
 import type { ServiceType } from './mergeServices'
 import { mergeServices } from './mergeServices'
 import { useWalletConnect } from 'plugins/walletConnectToDapps/WalletConnectBridgeContext'
-import { web3ByServiceType } from 'context/WalletProvider/web3byChainId'
-
-// info:  {
-//     _id: '6393a20355fd0ef62a97fb20',
-//     name: 'Binance Smart Chain Mainnet',
-//     type: 'EVM',
-//     tags: [
-//       'KeepKeySupport',
-//       'WalletConnectSupport',
-//       'DappSupport',
-//       'Binance Smart Chain Mainnet',
-//       'bnb',
-//       'BSC'
-//     ],
-//     blockchain: 'binance smart chain mainnet',
-//     symbol: 'BSC',
-//     service: 'https://bsc-dataseed2.binance.org',
-//     chainId: 56,
-//     network: [
-//       'https://bsc-dataseed1.binance.org',
-//       'https://bsc-dataseed2.binance.org',
-//       'https://bsc-dataseed3.binance.org',
-//       'https://bsc-dataseed4.binance.org',
-//       'https://bsc-dataseed1.defibit.io',
-//       'https://bsc-dataseed2.defibit.io',
-//       'https://bsc-dataseed3.defibit.io',
-//       'https://bsc-dataseed4.defibit.io',
-//       'https://bsc-dataseed1.ninicoin.io',
-//       'https://bsc-dataseed2.ninicoin.io',
-//       'https://bsc-dataseed3.ninicoin.io',
-//       'https://bsc-dataseed4.ninicoin.io',
-//       'wss://bsc-ws-node.nariox.org'
-//     ],
-//     facts: [
-//       {
-//         signer: '0x3f2329c9adfbccd9a84f52c906e936a42da18cb8',
-//         payload: '{"blockchain":"Binance Smart Chain Mainnet","chainId":56}',
-//         signature: '0x8e5bae56924978e0256ee46ae01c213a369234b7698328ffb78fef397f198624313ae65b12a4604dcc93bd7d3b343a407fbc051d9ade503faa0e091997e710b81c'
-//       }
-//     ],
-//     infoURL: 'https://www.binance.org',
-//     shortName: 'bnb',
-//     nativeCurrency: { name: 'Binance Chain Native Token', symbol: 'BNB', decimals: 18 },
-//     faucets: [ 'https://free-online-app.com/faucet-for-eth-evm-chains/' ]
-//   }
 
 export const ChainSelectorModal = () => {
   const [loading, setLoading] = useState(false)
@@ -109,7 +55,7 @@ export const ChainSelectorModal = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const useDebounce = (value, delay) => {
+  const useDebounce = (value: any, delay: number) => {
     // State and setters for debounced value
     const [debouncedValue, setDebouncedValue] = useState(value)
 
