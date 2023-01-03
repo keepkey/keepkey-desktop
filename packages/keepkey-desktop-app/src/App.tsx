640b9d2cf69007038262c1b3817eac617adc917f
Amitoj Singh <amitojsingh366@gmail.com>
2023-01-03 14:53:15 -0800

    fix skipping update

--- packages/keepkey-desktop-app/src/App.tsx
+++ packages/keepkey-desktop-app/src/App.tsx
@@ -109,6 +109,7 @@ export const App = () => {
               setConnected(true)
               const skipUpdate = deferred()
               skipUpdate.then(async () => {
+                await ipcListeners.keepkeySkipUpdate()
                 setIsUpdatingKeepkey(false)
                 updateKeepKey.close()
                 requestBootloaderMode.close()
