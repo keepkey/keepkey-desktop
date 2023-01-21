8875045c9328727e7fa686b36f44edf954059bd1
MrNerdHair <mrnerdhair@gmail.com>
2023-01-21 14:43:08 -0500

    csp updates

--- packages/keepkey-desktop-app/headers/csps/plugins/walletConnectToDapps.ts
+++ packages/keepkey-desktop-app/headers/csps/plugins/walletConnectToDapps.ts
@@ -1,5 +1,6 @@
 import type { Csp } from '../../types'
 
 export const csp: Csp = {
+  'connect-src': ['wss://relay.walletconnect.com'],
   'img-src': ['https://registry.walletconnect.com', 'https://explorer-api.walletconnect.com'],
 }
--- packages/keepkey-desktop-app/headers/csps/wallets/walletconnect.ts
+++ packages/keepkey-desktop-app/headers/csps/wallets/walletconnect.ts
@@ -5,7 +5,7 @@ export const csp: Csp = {
     '*',
     'wss://*.bridge.walletconnect.org/',
     'https://registry.walletconnect.com/api/v2/wallets',
-    "https://api.etherscan.io"
+    'https://api.etherscan.io',
   ],
-  'img-src': ['https://imagedelivery.net/', 'https://registry.walletconnect.com/api/v2/logo/', "*"],
+  'img-src': ['https://imagedelivery.net/', 'https://registry.walletconnect.com/api/v2/logo/', '*'],
 }
