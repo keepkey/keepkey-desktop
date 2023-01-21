acd630b22dfa07175188a6ef1d790200d559b91f
MrNerdHair <mrnerdhair@gmail.com>
2023-01-21 16:12:31 -0500

    disable authenticator button instead of hiding it

--- packages/keepkey-desktop-app/src/components/Layout/Header/NavBar/NavBar.tsx
+++ packages/keepkey-desktop-app/src/components/Layout/Header/NavBar/NavBar.tsx
@@ -8,7 +8,7 @@ import { union } from 'lodash'
 import { useEffect, useMemo, useState } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { Link as ReactRouterLink } from 'react-router-dom'
-import type { Route } from 'Routes/helpers'
+import type { Route, RouteCategory } from 'Routes/helpers'
 import { routes } from 'Routes/RoutesCommon'
 
 import { MainNavLink } from './MainNavLink'
@@ -27,31 +27,18 @@ export const NavBar = ({ isCompact, onClick, ...rest }: NavBarProps) => {
     state: { wallet },
   } = useWallet()
 
-  const handleClick = (onClick?: () => void) => {
-    onClick && onClick()
-  }
-
   const [supportsAuthenticator, setSupportsAuthenticator] = useState(false)
 
   const navItemGroups = useMemo(() => {
     const allRoutes = union(routes, pluginRoutes).filter(route => !route.disable && !route.hide)
-    const groups = allRoutes.reduce(
-      (entryMap, currentRoute) =>
-        entryMap.set(currentRoute.category, [
-          ...(entryMap.get(currentRoute.category) || []),
-          currentRoute,
-        ]),
-      new Map(),
-    )
+    const groups = allRoutes.reduce((entryMap, currentRoute) => {
+      if (!entryMap.has(currentRoute.category)) entryMap.set(currentRoute.category, [])
+      entryMap.get(currentRoute.category)!.push(currentRoute)
+      return entryMap
+    }, new Map<RouteCategory | undefined, Route[]>())
 
-    const entries = Array.from(groups.entries())
-
-    const index = entries[1][1].findIndex((entry: any) => entry.path === '/authenticator')
-
-    !supportsAuthenticator && entries[1][1].splice(index, 1)
-
-    return entries
-  }, [pluginRoutes, supportsAuthenticator])
+    return Array.from(groups.entries())
+  }, [pluginRoutes])
 
   useEffect(() => {
     wallet?.getFirmwareVersion().then(version => {
@@ -62,28 +49,34 @@ export const NavBar = ({ isCompact, onClick, ...rest }: NavBarProps) => {
 
   return (
     <Flex width='full' flexDir='row' gap={6} {...rest}>
-      {navItemGroups.map((group, _id) => {
+      {navItemGroups.map((group, groupId) => {
         const [, values] = group
-        return values.map((item: Route, id: number) => (
-          <MainNavLink
-            isCompact={isCompact}
-            as={ReactRouterLink}
-            key={id}
-            leftIcon={item.icon}
-            href={item.path}
-            to={item.path}
-            size='lg'
-            onClick={onClick}
-            label={translate(item.label)}
-            aria-label={translate(item.label)}
-            data-test={`navigation-${item.label.split('.')[1]}-button`}
-          />
-        ))
+        return values
+          .filter((route: Route) => !route.hide)
+          .map((item: Route, itemId: number) => {
+            const disabled = item.path === '/authenticator' && !supportsAuthenticator
+            return (
+              <MainNavLink
+                isCompact={isCompact}
+                as={!disabled ? ReactRouterLink : undefined}
+                key={`${groupId}-${itemId}`}
+                leftIcon={item.icon}
+                href={item.path}
+                to={item.path}
+                size='lg'
+                onClick={onClick}
+                label={translate(item.label)}
+                disabled={disabled}
+                aria-label={translate(item.label)}
+                data-test={`navigation-${item.label.split('.')[1]}-button`}
+              />
+            )
+          })
       })}
       <MainNavLink
         isCompact={isCompact}
         size='sm'
-        onClick={() => handleClick(() => settings.open({}))}
+        onClick={() => settings.open({})}
         label={translate('common.settings')}
         leftIcon={<SettingsIcon />}
         data-test='navigation-settings-button'
@@ -98,7 +91,6 @@ export const NavBar = ({ isCompact, onClick, ...rest }: NavBarProps) => {
         leftIcon={<ChatIcon />}
         data-test='navigation-join-discord-button'
       />
-      {/* {isYatFeatureEnabled && <YatBanner isCompact={isCompact} />} */}
     </Flex>
   )
 }
