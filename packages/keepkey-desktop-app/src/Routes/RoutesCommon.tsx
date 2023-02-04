c53af1b4488ad284406930e3eead79034390ba77
highlander <bithighlander@gmail.com>
2023-02-04 15:56:58 -0600

    full dapp pt2

--- packages/keepkey-desktop-app/src/Routes/RoutesCommon.tsx
+++ packages/keepkey-desktop-app/src/Routes/RoutesCommon.tsx
@@ -1,22 +1,22 @@
-import { AccountsIcon } from 'components/Icons/Accounts'
+// import { AccountsIcon } from 'components/Icons/Accounts'
 import { AssetsIcon } from 'components/Icons/Assets'
-import { DashboardIcon } from 'components/Icons/Dashboard'
-import { TxHistoryIcon } from 'components/Icons/TxHistory'
-import { Account } from 'pages/Accounts/Account'
-import { Accounts } from 'pages/Accounts/Accounts'
-import { AccountToken } from 'pages/Accounts/AccountToken/AccountToken'
-import { AccountTokenTxHistory } from 'pages/Accounts/AccountToken/AccountTokenTxHistory'
-import { AccountTxHistory } from 'pages/Accounts/AccountTxHistory'
+// import { DashboardIcon } from 'components/Icons/Dashboard'
+// import { TxHistoryIcon } from 'components/Icons/TxHistory'
+// import { Account } from 'pages/Accounts/Account'
+// import { Accounts } from 'pages/Accounts/Accounts'
+// import { AccountToken } from 'pages/Accounts/AccountToken/AccountToken'
+// import { AccountTokenTxHistory } from 'pages/Accounts/AccountToken/AccountTokenTxHistory'
+// import { AccountTxHistory } from 'pages/Accounts/AccountTxHistory'
 import { Asset } from 'pages/Assets/Asset'
 import { Assets } from 'pages/Assets/Assets'
 import { AssetTxHistory } from 'pages/Assets/AssetTxHistory'
 import { KeepkeyAsset } from 'pages/Assets/KeepkeyAsset'
 import { Authenticator } from 'pages/Authenticator/Authenticator'
 import { Browser } from 'pages/Browser/Browser'
-import { Dashboard } from 'pages/Dashboard/Dashboard'
+// import { Dashboard } from 'pages/Dashboard/Dashboard'
 import { PairingDetails } from 'pages/Pairings/PairingDetails'
 import { Pairings } from 'pages/Pairings/Pairings'
-import { TransactionHistory } from 'pages/TransactionHistory/TransactionHistory'
+// import { TransactionHistory } from 'pages/TransactionHistory/TransactionHistory'
 import { WalletConnectToDapps } from 'plugins/walletConnectToDapps/WalletConnectToDapps'
 import { FaBuromobelexperte, FaGlobe, FaLock, FaPlug } from 'react-icons/fa'
 
@@ -31,19 +31,26 @@ export const routes: NestedRoute[] = [
     icon: <FaBuromobelexperte />,
     category: RouteCategory.Wallet,
   },
+  // {
+  //   path: '/dashboard',
+  //   label: 'navBar.dashboard',
+  //   icon: <DashboardIcon />,
+  //   main: Dashboard,
+  //   category: RouteCategory.Wallet,
+  // },
+  // {
+  //   path: '/transaction-history',
+  //   label: 'navBar.transactionHistory',
+  //   icon: <TxHistoryIcon />,
+  //   main: TransactionHistory,
+  //   category: RouteCategory.Explore,
+  // },
   {
-    path: '/dashboard',
-    label: 'navBar.dashboard',
-    icon: <DashboardIcon />,
-    main: Dashboard,
-    category: RouteCategory.Wallet,
-  },
-  {
-    path: '/transaction-history',
-    label: 'navBar.transactionHistory',
-    icon: <TxHistoryIcon />,
-    main: TransactionHistory,
+    path: '/authenticator',
+    label: 'navBar.authenticator',
+    icon: <FaLock />,
     category: RouteCategory.Explore,
+    main: Authenticator,
   },
   {
     path: '/browser',
@@ -67,13 +74,6 @@ export const routes: NestedRoute[] = [
         hide: true,
       },
     ],
-  },
-  {
-    path: '/authenticator',
-    label: 'navBar.authenticator',
-    icon: <FaLock />,
-    category: RouteCategory.Explore,
-    main: Authenticator,
   },
   {
     path: '/assets',
@@ -117,51 +117,51 @@ export const routes: NestedRoute[] = [
       },
     ],
   },
-  {
-    path: '/accounts',
-    label: 'navBar.accounts',
-    main: Accounts,
-    icon: <AccountsIcon />,
-    category: RouteCategory.Explore,
-    routes: [
-      {
-        path: '/:accountId',
-        label: 'Account Details',
-        main: null,
-        hide: true,
-        routes: [
-          {
-            path: '/',
-            label: 'navBar.overview',
-            main: Account,
-          },
-          {
-            path: '/transactions',
-            label: 'navBar.transactions',
-            main: AccountTxHistory,
-          },
-          {
-            path: '/:assetId',
-            label: 'navBar.overview',
-            main: null,
-            hide: true,
-            routes: [
-              {
-                path: '/',
-                main: AccountToken,
-                label: 'navBar.overview',
-              },
-              {
-                path: '/transactions',
-                main: AccountTokenTxHistory,
-                label: 'navBar.transactions',
-              },
-            ],
-          },
-        ],
-      },
-    ],
-  },
+  // {
+  //   path: '/accounts',
+  //   label: 'navBar.accounts',
+  //   main: Accounts,
+  //   icon: <AccountsIcon />,
+  //   category: RouteCategory.Explore,
+  //   routes: [
+  //     {
+  //       path: '/:accountId',
+  //       label: 'Account Details',
+  //       main: null,
+  //       hide: true,
+  //       routes: [
+  //         {
+  //           path: '/',
+  //           label: 'navBar.overview',
+  //           main: Account,
+  //         },
+  //         {
+  //           path: '/transactions',
+  //           label: 'navBar.transactions',
+  //           main: AccountTxHistory,
+  //         },
+  //         {
+  //           path: '/:assetId',
+  //           label: 'navBar.overview',
+  //           main: null,
+  //           hide: true,
+  //           routes: [
+  //             {
+  //               path: '/',
+  //               main: AccountToken,
+  //               label: 'navBar.overview',
+  //             },
+  //             {
+  //               path: '/transactions',
+  //               main: AccountTokenTxHistory,
+  //               label: 'navBar.transactions',
+  //             },
+  //           ],
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
   // {
   //   path: '/flags',
   //   label: 'navBar.featureFlags',
