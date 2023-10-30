50e8a34de12ab3ccc6555f47320a378cacd871f1
bithighlander <bithighlander@gmail.com>
2023-10-30 10:37:24 -0600

    bumped in sdk

--- packages/keepkey-sdk-server/src/controllers/ethereum.ts
+++ packages/keepkey-sdk-server/src/controllers/ethereum.ts
@@ -116,18 +116,19 @@ export class EthereumController extends ApiController {
       insight = insight.data
       console.log('insight: ', insight)
       console.log('insight.recommended: ', insight.recommended)
-      if (insight.recommended.maxFeePerGas) {
-        msg.maxFeePerGas = insight.recommended.maxFeePerGas
-      }
-      if (insight.recommended.gasPrice) {
-        msg.gasPrice = insight.recommended.gasPrice
-      }
-      if (insight.recommended.gasLimit) {
-        msg.gasLimit = insight.recommended.gasLimit
-      }
-      if (insight.recommended.maxPriorityFeePerGas) {
-        msg.maxPriorityFeePerGas = insight.recommended.maxPriorityFeePerGas
-      }
+      //Nerving insight (Move to pre-send)
+      // if (insight.recommended.maxFeePerGas) {
+      //   msg.maxFeePerGas = insight.recommended.maxFeePerGas
+      // }
+      // if (insight.recommended.gasPrice) {
+      //   msg.gasPrice = insight.recommended.gasPrice
+      // }
+      // if (insight.recommended.gasLimit) {
+      //   msg.gasLimit = insight.recommended.gasLimit
+      // }
+      // if (insight.recommended.maxPriorityFeePerGas) {
+      //   msg.maxPriorityFeePerGas = insight.recommended.maxPriorityFeePerGas
+      // }
     } catch (e) {
       console.error('unable to get tx insight', e)
     }
@@ -196,7 +197,7 @@ export class EthereumController extends ApiController {
     return (
       await this.context.wallet.ethSignMessage({
         addressNList: account.addressNList,
-        message: Buffer.from(body.message.replace(/^0x/, ''), 'hex'),
+        message: Buffer.from(body.message.replace(/^0x/, ''), 'hex').toString(),
       })
     ).signature
   }
