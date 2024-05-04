e397b4eba757b1f815811e83ae9e937a2f6c9034
bithighlander <bithighlander@gmail.com>
2024-05-04 16:54:32 -0500

    refactor

--- packages/keepkey-desktop-app/src/pages/Browser/Browser.tsx
+++ packages/keepkey-desktop-app/src/pages/Browser/Browser.tsx
@@ -1,21 +1,26 @@
-import { Main } from 'components/Layout/Main'
-
-import { Webview } from './Webview';
+// Browser.tsx
+import { Main } from 'components/Layout/Main';
+import Webview from './Webview';
+import { useDisclosure } from '@chakra-ui/react';
 
 export const Browser = () => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
 
-  return (
-      <Main
-          height='full'
-          style={{
-            padding: 0,
-            minWidth: '100%',
-            flexGrow: 1,
-            display: 'flex',
-            flexDirection: 'column',
-          }}
-      >
-        <Webview />
-      </Main>
-  )
-}
+    return (
+        <Main
+            height='full'
+            style={{
+                padding: 0,
+                minWidth: '100%',
+                flexGrow: 1,
+                display: 'flex',
+                flexDirection: 'column',
+            }}
+        >
+            <Webview id="primary-webview" initialUrl="https://private.shapeshift.com/" isOpen={isOpen} onToggleOpen={onOpen} />
+            {isOpen && (
+                <Webview id="secondary-webview" initialUrl="https://wallet-connect-dapp-ochre.vercel.app/" isOpen={isOpen} onToggleOpen={onClose} />
+            )}
+        </Main>
+    );
+};
