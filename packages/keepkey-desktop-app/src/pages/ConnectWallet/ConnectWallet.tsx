cb08fce34a78c22cba7b7d7dd1392656ffb40fcc
highlander <bithighlander@gmail.com>
2025-04-13 12:15:18 -0500

    desktop work

--- packages/keepkey-desktop-app/src/pages/ConnectWallet/ConnectWallet.tsx
+++ packages/keepkey-desktop-app/src/pages/ConnectWallet/ConnectWallet.tsx
@@ -1,7 +1,7 @@
 import { DarkMode } from '@chakra-ui/color-mode'
 import { ExternalLinkIcon } from '@chakra-ui/icons'
 import { Flex, Link } from '@chakra-ui/layout'
-import { Button, Image } from '@chakra-ui/react'
+import { Button, Image, Box, Code, Text as ChakraText, useClipboard, HStack, VStack, Divider } from '@chakra-ui/react'
 import logo from 'assets/kk-icon-gold.png'
 import heroBgImage from 'assets/splash-bg.png'
 import { Page } from 'components/Layout/Page'
@@ -10,7 +10,7 @@ import { ipcListeners } from 'electron-shim'
 import { useModal } from 'hooks/useModal/useModal'
 import { useQuery } from 'hooks/useQuery/useQuery'
 import { useWallet } from 'hooks/useWallet/useWallet'
-import { useEffect } from 'react'
+import { useEffect, useState, useCallback } from 'react'
 import { useTranslate } from 'react-polyglot'
 import { generatePath, matchPath, useHistory } from 'react-router'
 
@@ -20,12 +20,41 @@ export const ConnectWallet = () => {
   const history = useHistory()
   const translate = useTranslate()
   const query = useQuery<{ returnUrl: string }>()
+  const [serviceKey, setServiceKey] = useState<string>('')
+  const { onCopy, hasCopied } = useClipboard(serviceKey)
+  const [copiedText, setCopiedText] = useState(false)
 
   const { onboardingSteps, keepKeyWipe } = useModal()
 
   const debugDevice = async function () {
     await ipcListeners.appRestart()
   }
+
+  const openApiDocs = () => {
+    window.open('http://localhost:1646/docs/#/', '_blank')
+  }
+
+  // Manual copy implementation to ensure the correct value is copied
+  const copyServiceKey = useCallback(() => {
+    if (serviceKey) {
+      navigator.clipboard.writeText(serviceKey)
+        .then(() => {
+          setCopiedText(true)
+          setTimeout(() => setCopiedText(false), 2000) // Reset after 2 seconds
+        })
+        .catch(err => {
+          console.error('Failed to copy: ', err)
+        })
+    }
+  }, [serviceKey])
+
+  useEffect(() => {
+    // Load service key from localStorage
+    const key = window.localStorage.getItem('@app/serviceKey')
+    if (key) {
+      setServiceKey(key)
+    }
+  }, [])
 
   useEffect(() => {
     // This handles reloading an asset's account page on Native/KeepKey. Without this, routing will break.
@@ -63,7 +92,51 @@ export const ConnectWallet = () => {
           height='100%'
           alignItems='center'
           justifyContent='center'
+          position="relative"
         >
+          {/* API Key Display Box - Bottom Right */}
+          {serviceKey && (
+            <Box 
+              position="absolute"
+              bottom="8"
+              right="8"
+              zIndex="10"
+              borderRadius="md" 
+              border="1px solid" 
+              borderColor="gray.600" 
+              p={4}
+              bg="gray.800"
+              width="300px"
+              boxShadow="xl"
+            >
+              <VStack spacing={3} align="stretch">
+                <ChakraText color="white" fontWeight="bold">
+                  API Service Key:
+                </ChakraText>
+                <Code 
+                  width="100%" 
+                  bg="gray.700" 
+                  color="green.300" 
+                  fontSize="sm" 
+                  p={2} 
+                  borderRadius="md"
+                  overflowX="auto"
+                >
+                  {serviceKey}
+                </Code>
+                
+                <HStack spacing={2} mt={2}>
+                  <Button size="sm" flex={1} onClick={copyServiceKey} colorScheme="blue">
+                    {copiedText ? "Copied!" : "Copy Key"}
+                  </Button>
+                  <Button size="sm" flex={1} onClick={openApiDocs} colorScheme="teal">
+                    View API Docs
+                  </Button>
+                </HStack>
+              </VStack>
+            </Box>
+          )}
+          
           <Flex
             flexDir='column'
             alignItems='center'
@@ -76,81 +149,80 @@ export const ConnectWallet = () => {
               flex={1}
               flexDir='column'
               justifyContent='center'
+              alignItems='center'
               height='100vh'
-              width='60%'
+              width='100%'
               px={6}
             >
-              <Image objectFit='cover' width='70px' src={logo} />
-              <Flex flexDir='row' letterSpacing='-2px' my={6}>
-                <RawText
-                  textAlign='left'
-                  color='white'
-                  width='80%'
-                  fontWeight='light'
-                  lineHeight={1}
-                  fontSize='6xl'
-                  userSelect={'none'}
-                >
-                  {translate('connectWalletPage.nextFrontier')}
-                </RawText>
+              <Flex 
+                direction="column" 
+                maxWidth="600px"
+                width="100%"
+              >
+                <Image objectFit='cover' width='70px' src={logo} />
+                <Flex flexDir='row' letterSpacing='-2px' my={6}>
+                  <RawText
+                    textAlign='left'
+                    color='white'
+                    width='80%'
+                    fontWeight='light'
+                    lineHeight={1}
+                    fontSize='6xl'
+                    userSelect={'none'}
+                  >
+                    {translate('connectWalletPage.nextFrontier')}
+                  </RawText>
+                </Flex>
               </Flex>
-              {/*<Text*/}
-              {/*  userSelect={'none'}*/}
-              {/*  color='white'*/}
-              {/*  fontSize='lg'*/}
-              {/*  mb={12}*/}
-              {/*  textAlign='left'*/}
-              {/*  translation='connectWalletPage.protectYourCrypto'*/}
-              {/*/>*/}
-            </Flex>
-            <Flex
-              direction={'column'}
-              gap={4}
-              width='full'
-              position={{ base: 'static', md: 'fixed' }}
-              zIndex={3}
-              py={3}
-              px={4}
-              bottom={20}
-              alignItems={'center'}
-            >
-              <Button
-                as={Link}
-                isExternal
-                width='360px'
-                href='https://keepkey.myshopify.com/'
-                rightIcon={<ExternalLinkIcon />}
-                colorScheme='green'
+              
+              {/* Main Action Buttons - Centered */}
+              <Flex
+                direction='column'
+                gap={4}
+                width='100%'
+                maxWidth="400px"
+                zIndex={3}
+                mt={10}
+                alignItems='center'
               >
-                {translate('connectWalletPage.buyKeepKey')}
-              </Button>
-              <Button
-                width='360px'
-                rightIcon={<ExternalLinkIcon />}
-                colorScheme='green'
-                onClick={debugDevice}
-              >
-                {translate('connectWalletPage.troubleshoot')}
-              </Button>
-              <Button
-                width='360px'
-                rightIcon={<ExternalLinkIcon />}
-                colorScheme='green'
-                onClick={() => keepKeyWipe.open({})}
-              >
-                Wipe keepkey
-              </Button>
-              <Button
-                as={Link}
-                isExternal
-                href='https://discord.gg/stfRnW3Jys'
-                width='360px'
-                size='lg'
-                rightIcon={<ExternalLinkIcon />}
-                colorScheme='green'
-              >
-                <Text translation={'common.getSupport'} />
-              </Button>
+                <Button
+                  as={Link}
+                  isExternal
+                  width='360px'
+                  href='https://keepkey.myshopify.com/'
+                  rightIcon={<ExternalLinkIcon />}
+                  colorScheme='green'
+                >
+                  {translate('connectWalletPage.buyKeepKey')}
+                </Button>
+                <Button
+                  width='360px'
+                  rightIcon={<ExternalLinkIcon />}
+                  colorScheme='green'
+                  onClick={debugDevice}
+                >
+                  {translate('connectWalletPage.troubleshoot')}
+                </Button>
+                <Button
+                  width='360px'
+                  rightIcon={<ExternalLinkIcon />}
+                  colorScheme='green'
+                  onClick={() => keepKeyWipe.open({})}
+                >
+                  Wipe keepkey
+                </Button>
+                <Button
+                  as={Link}
+                  isExternal
+                  href='https://discord.gg/stfRnW3Jys'
+                  width='360px'
+                  size='lg'
+                  rightIcon={<ExternalLinkIcon />}
+                  colorScheme='green'
+                >
+                  <Text translation={'common.getSupport'} />
+                </Button>
+              </Flex>
             </Flex>
           </Flex>
         </Flex>
